883905d493ba72cf6c9f256d5d73181866851735
ZombieCloud <konstantin.gladky@gmail.com>
2015-11-13 00:48:26 +0200

    comm4
    заработало на сервисе

--- 2/Words/app/src/main/java/com/app/words/MainService.java
+++ 2/Words/app/src/main/java/com/app/words/MainService.java
@@ -2,15 +2,25 @@ package com.app.words;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class MainService extends Service {
 
-    final String LOG_TAG = "myLogs";
-    int n;   //Переменная-флаг, которая остановит поток. (По-людски поток вообще остановить нельзя?)
+    final String LOG_TAG = "wordLogs";
+    int n;     // Счетчик слов
+    boolean stop_thread;   // Переменная-флаг, которая остановит поток. (По-людски поток вообще остановить нельзя?)
+    MediaPlayer mediaPlayer;
+    Uri myUri;
+    Word newWord;
+
 
     public MainService() {
     }
@@ -19,22 +29,30 @@ public class MainService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(LOG_TAG, "onCreate");
+        stop_thread = false;
     }
 
 
-    // Срабатывает, когда сервис запущен методом "startService". В нем мы запускаем метод "someTask"
+
+    // Срабатывает, когда сервис запущен методом "startService". В нем мы запускаем то, что нам нужно
     // У "onStartCommand" на вход и на выход идут параметры
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOG_TAG, "onStartCommand");
+        Log.d(LOG_TAG, "startNum = " + intent.getStringExtra("startNum"));
+        Log.d(LOG_TAG, "lastNum = " + intent.getStringExtra("lastNum"));
+        Log.d(LOG_TAG, "Interval = " + intent.getStringExtra("Interval"));
 
-        // Достать/произнести слово
-        OneWord();
+        // Перебор слов. Номера первого и последнего слов и интервал приезжают сюда вместе с intent
+        Start_Fetching_Of_The_Words(Integer.valueOf(intent.getStringExtra("startNum")),     Integer.valueOf(intent.getStringExtra("lastNum")),     Integer.valueOf(intent.getStringExtra("Interval")));
 
         return super.onStartCommand(intent, flags, startId);
     }
 
+
+
+
     public void onDestroy() {
-        n = 1000001;   //Это остановит поток
+        stop_thread = true;   //Это остановит поток
         super.onDestroy();
         Log.d(LOG_TAG, "onDestroy");
     }
@@ -54,21 +72,90 @@ public class MainService extends Service {
 
 
     // Достать/произнести слово. Запускаем в отдельном потоке
-    void OneWord() {
+    void Start_Fetching_Of_The_Words(final int startNum,   final int lastNum,   final int interval) {
         new Thread(new Runnable() {
             public void run() {
-                n = 0;
-                while (n < 1000000) {
-                    n++;
+
+                n = startNum;
+                while ((n >= startNum) && (n <= lastNum)) {
                     Log.d(LOG_TAG, "n = " + n);
-                    try {
-                        TimeUnit.SECONDS.sleep(5);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+
+                    //Достать новое слово
+                    newWord = new Word(String.valueOf(n));
+
+//                    _textView.setText(newWord._ru);
+                    PlayWord(newWord._ruSound);
+                    Pause(interval);
+
+//                    _textView.setText(newWord._en);
+                    PlayWord(newWord._enSound);
+                    Pause(interval);
+
+                    newWord = null;
+
+                    KillPlayer();
+
+                    //Следующее слово
+                    n++;
+
+                    // Заново
+                    if (n > lastNum) n = startNum;
+
+                    //Стоп потоку
+                    if (stop_thread) break;
                 }
                 stopSelf();  // Останавливает сервис, в котором был вызван поток
             }
         }).start();
     }
+
+
+
+    //Произнести слово
+    private void PlayWord(File fileToPlay) {
+
+        KillPlayer();
+
+        //Новый плеер
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            myUri = Uri.parse(fileToPlay.getAbsolutePath());             // "/mnt/sdcard/app_words/en_1.wav"
+            mediaPlayer.setDataSource(getApplicationContext(), myUri);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            KillPlayer();
+        }
+    }
+
+
+
+    //Пауза
+    private void Pause(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //Попытаемся убить плеер. Вдруг играет
+    private void KillPlayer() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            } catch (Exception e) {
+                Log.d(LOG_TAG, "Can't kill player");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
---
+++ 2/Words/app/src/main/java/com/app/words/Word.java
@@ -0,0 +1,189 @@
+package com.app.words;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class Word {
+
+    final String LOG_TAG = "wordLogs";
+
+    public String _en = "";
+    public String _ru = "";
+    public File _enSound;
+    public File _ruSound;
+
+
+
+    //Конструктор
+    public Word(String num) {
+        String filename;
+
+        URL url;
+        HttpURLConnection urlConnection = null;
+        InputStream in;
+        int data;
+        InputStreamReader isw;
+        _ru = "Something ru";
+        _en = "Something en";
+
+
+
+        // Слово EN (_en) ------------------
+        try {
+            url = new URL("http://tests.progmans.net/index.php?NUM_EN=" + num);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = urlConnection.getInputStream();
+            isw = new InputStreamReader(in);
+            data = isw.read();
+            while (data != -1) {
+                char currentSymbol = (char) data;
+                data = isw.read();
+                _en = _en + currentSymbol;
+            }
+
+        } catch (Exception e) {     //  Нет интернета
+            _en = "Something wrong with internet :(  en_";
+            Log.d(LOG_TAG, "Something wrong with internet :(  en_");
+            e.printStackTrace();
+        }  finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+
+
+        // Слово RU (_ru) -----------------------
+        try {
+            url = new URL("http://tests.progmans.net/index.php?NUM_RU=" + num);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = urlConnection.getInputStream();
+            isw = new InputStreamReader(in);
+            data = isw.read();
+            while (data != -1) {
+                char currentSymbol = (char) data;
+                data = isw.read();
+                _ru = _ru + currentSymbol;
+            }
+
+        } catch (Exception e) {     // Нет интернета
+            _ru = "Something wrong with internet :(  ru_";
+            Log.d(LOG_TAG, "Something wrong with internet :(  ru_");
+            e.printStackTrace();
+        }  finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+
+
+        // Звуки качаем только если локальное хранилище доступно
+        if (isExternalStorageWritable()) {
+
+            // Звук EN (_enSound) --------------------------------
+            try {
+                filename = "en_" + num + ".wav";
+                _enSound = new File(getSoundStorageDir("app_words"), filename);
+                if (!_enSound.exists()) {
+                    url = new URL("http://tests.progmans.net/index.php?NUM_EN_SOUND=" + num);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    in = urlConnection.getInputStream();
+                    copyInputStreamToFile(in, _enSound);
+                }
+
+            } catch (Exception e) {    // Нет интернета
+                _en = "Something wrong with sound files or internet:(";
+                _enSound = null;
+                Log.d(LOG_TAG, "Something wrong with sound files or internet:(");
+                e.printStackTrace();
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+
+
+            // Звук RU (_ruSound) -----------------------------
+            try {
+                filename = "ru_" + num + ".wav";
+                _ruSound = new File(getSoundStorageDir("app_words"), filename);
+                if (!_ruSound.exists()) {
+                    url = new URL("http://tests.progmans.net/index.php?NUM_RU_SOUND=" + num);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    in = urlConnection.getInputStream();
+                    copyInputStreamToFile(in, _ruSound);
+                }
+
+            } catch (Exception e) {    // Нет интернета
+                _ru = "Something wrong with sound files or internet:(";
+                _ruSound = null;
+                Log.d(LOG_TAG, "Something wrong with sound files or internet:(");
+                e.printStackTrace();
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+
+        } else {
+            Log.d(LOG_TAG, "Storage is not available");
+            _en = "Storage is not available :(";
+            _ru = "Storage is not available :(";
+        }
+    }
+
+
+
+
+    // Get the directory for the app's private pictures directory.
+    public File getSoundStorageDir(String albumName) {
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), albumName);
+        if (!file.mkdirs()) {
+            Log.d(LOG_TAG, "Directory not created");
+        }
+        return file;
+    }
+
+
+
+//     Checks if external storage is available for read and write
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    //Копирует InputStream из URL в файл (http://stackoverflow.com/questions/10854211/android-store-inputstream-in-file)
+    private void copyInputStreamToFile(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len = in.read(buf)) > 0){
+                out.write(buf, 0, len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
